9b0c45d7eced823468928406972293decd5cc491
Mike Zurzolo <mike@hsowen.com>
2019-04-12 13:05:43 -0400

    Test more stuff

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -6,9 +6,9 @@ def test_t_flag():
     CCC.codechecker.main(['-t'])
 
 def test_dr_flags():
-    CCC.codechecker.main(['-d', '/home/travis/virtualenv'])
-    CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv'])
-    CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv'])
+    CCC.codechecker.main(['-d', '/home/travis/'])
+    CCC.codechecker.main(['-r', '-d', '/home/travis/'])
+    CCC.codechecker.main(['-r', '-d', '/home/travis/'])
 
 def test_purge():
     CCC.codechecker.main(['-p'])
