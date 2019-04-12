32b633cda3c975de79ed637a78088d81f694f3af
Mike Zurzolo <mike@hsowen.com>
2019-04-11 21:59:46 -0400

    Force loading behavior in testing

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -2,13 +2,12 @@ import unittest
 import pytest
 import clonedcodechecker as CCC
 
-def test_recursive():
-    CCC.codechecker.main(['-r'])
-
 def test_t_flag():
     CCC.codechecker.main(['-t'])
 
 def test_dr_flags():
+    CCC.codechecker.main(['-d', '/home/travis/virtualenv'])
+    CCC.codechecker.main(['-r', '/home/travis/virtualenv'])
     CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv'])
 
 def test_purge():
