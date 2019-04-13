62e8a98f3847df666edc029b475f00082845572c
Mike Zurzolo <mike@hsowen.com>
2019-04-13 08:02:01 -0400

    Pattern closer to catching functions/member functions, still catches some other forms. Line counting still needs attention.

--- clonedcodechecker/matcher.py
+++ clonedcodechecker/matcher.py
@@ -46,7 +46,7 @@ class Matcher:
         double_slash_comment = r'//.*?\n'      # comment up to newline
         slash_star_comment = r'/\*.*?\*/'       # comment open to close
         # Containers (more)
-        brace_c = r'.*?\{.*?\}'
+        brace_c = r'[ \t]*?[A-Za-z]+[ \t]+[A-Za-z]+[ \t]*?\(.*?\)[ \t]*?\{.*?\}'
         newline = r'\n'
         other = r'.'
         token_specification = [
