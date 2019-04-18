9ead57398477cdc4a0c648b680d86fb245d4ce1e
Mike Zurzolo <mike@hsowen.com>
2019-04-17 22:10:21 -0400

    last try of the night...

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -2,8 +2,6 @@ import unittest
 import pytest
 import clonedcodechecker as CCC
 
-def test_t_flag():
-
 def test_dr_flags():
     CCC.codechecker.main(['-d', '/home/travis/virtualenv'])
     CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv'])
