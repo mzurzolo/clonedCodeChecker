26a4559df2a98d625a9ff2c67e9972a1ad63f3af
Mike Zurzolo <mike@hsowen.com>
2019-04-19 15:33:15 -0400

    build too long...

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -3,9 +3,9 @@ import pytest
 import clonedcodechecker as CCC
 
 def test_dr_flags():
-    CCC.codechecker.main(['-d', '/home/travis/virtualenv'])
-    CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv'])
-    CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv'])
+    CCC.codechecker.main(['-d', '/home/travis/virtualenv/linux/arch'])
+    CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv/linux/arch'])
+    CCC.codechecker.main(['-r', '-d', '/home/travis/virtualenv/linux/arch'])
 
 def test_purge():
     CCC.codechecker.main(['-p'])
