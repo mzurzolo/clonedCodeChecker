c2f3cc892abd1dc0b469a0f00edc61df2659c46b
Mike Zurzolo <mike@hsowen.com>
2019-04-24 12:35:43 -0400

    When does docker die? 4

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -1,10 +1,15 @@
 import unittest
 import pytest
+import os
 import clonedcodechecker as CCC
 
 def test_dr_flags():
     CCC.codechecker.main(['-d', '../opencv'])
     CCC.codechecker.main(['-r', '-d', '../opencv'])
+    CCC.codechecker.main(['-r', '-d', '../opencv'])
+    for fname in os.listdir('../opencv'):
+        with open('../opencv/' + fname, 'a') as file:
+            print('1', file=file)
     CCC.codechecker.main(['-r', '-d', '../opencv'])
 
 def test_purge():
