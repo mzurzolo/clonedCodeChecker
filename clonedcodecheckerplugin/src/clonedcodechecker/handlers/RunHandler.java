d49e3a7f098166cb3c7960851f81c2f9d28942ef
Mike Zurzolo <mike@hsowen.com>
2019-05-03 17:38:23 -0400

    more java config and testing

--- clonedcodecheckerplugin/src/clonedcodechecker/handlers/RunHandler.java
+++ clonedcodecheckerplugin/src/clonedcodechecker/handlers/RunHandler.java
@@ -1,17 +1,14 @@
 package clonedcodechecker.handlers;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.FileReader;
-import java.io.BufferedReader;
 
 import javax.inject.Named;
+
+import clonedcodechecker.handlers.InternalInputDialog;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.dialogs.PopupDialog;
 import org.eclipse.swt.widgets.Shell;
 
 /** <b>Warning</b> :
@@ -19,48 +16,60 @@ As explained in <a href="http://wiki.eclipse.org/Eclipse4/RCP/FAQ#Why_aren.27t_m
 <b>Inject the values in the @Execute methods</b>
 */
 public class RunHandler {
+	private InternalInputDialog inputdialog;
+	private String init_string = System.getProperty("user.home");
 
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell s) {
-
-		InputDialog inputdialog = new InputDialog(s,
-				"Run Cloned Code Checker",
-				"Please enter a directory to check:",
-				System.getProperty("user.home"), null);
-		inputdialog.open();
-		String entered_directory = inputdialog.getValue();
-		ProcessBuilder processBuilder = new ProcessBuilder();
-		String commandString = "ccc" + " -rjd " + entered_directory;
-		processBuilder.command("bash", "-c", commandString + " &");
+		
+		
+		this.setInputDialog(s);
+		this.openInputDialog();
+		String inputstring = this.getInput();
+		ProcessBuilder pBuilder = this.getprocessBuilder(inputstring);
+		
 		try {
-
-			Process process = processBuilder.start();
-
-			StringBuilder output = new StringBuilder();
-
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(process.getInputStream()));
-
-			String line;
-			while ((line = reader.readLine()) != null) {
-				//output.append(line + "\n");
-			}
-
-			int exitVal = process.waitFor();
-			if (exitVal == 0) {
-				System.out.println("Success!");
-				System.out.println(output);
-			} else {
-				//abnormal...
-			}
-
-
+			this.startProcess(pBuilder);
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void setInputDialog(Shell s) {
+		this.inputdialog = new InternalInputDialog(s,
+				"Run Cloned Code Checker",
+				"Please enter a directory to check:",
+				this.init_string, null);
+	}
+	
+	public void openInputDialog() {
+		this.inputdialog.open();
+	}
+		
+	public void closeDialog() {
+		this.inputdialog.close();
+	}
+	
+	public String getInput() {
+		return this.inputdialog.getValue();
+	}
+	
+	public ProcessBuilder getprocessBuilder(String entered_directory) {
+		ProcessBuilder processBuilder = new ProcessBuilder().inheritIO();
+		String commandString = "ccc" + " -rjd " + entered_directory;
+		processBuilder.command("bash", "-c", commandString);
+		return processBuilder;
+	}
+	
+	public void startProcess(ProcessBuilder processBuilder) throws IOException {
+		processBuilder.start();
+	}
+	
+	public void setInitString(String s) {
+		this.init_string = s;
+	}
+	
+	
 
 
 
