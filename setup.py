0121acced140c5ce844801c73075556332449716
Mike Zurzolo <mike@hsowen.com>
2019-05-08 10:11:07 -0400

    last one

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def main():
           install_requires=['ruamel.yaml'],
           entry_points={
               'console_scripts': [
-                  'ccc = clonedcodechecker.codechecker:run'
+                  'ccc = clonedcodechecker.codechecker:main'
               ]
           })
 
