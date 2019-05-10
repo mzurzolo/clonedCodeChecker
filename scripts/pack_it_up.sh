a1a16d8024833982629e16a229b509a1d15c4fea
Mike Zurzolo <mike@hsowen.com>
2019-05-10 14:56:14 -0400

    releases fix

--- scripts/pack_it_up.sh
+++ scripts/pack_it_up.sh
@@ -9,7 +9,7 @@ mkdir RELEASES/ccc.latest
 source scripts/work_here.sh
 mvn -f clonedcodecheckerplugin clean package
 python3 setup.py sdist bdist bdist_wheel
-python3 -m pip download -r requirements.txt -d RELEASES/.ccc.latest/
+python3 -m pip download -r requirements.txt -d RELEASES/ccc.latest/
 
 mv clonedcodecheckerplugin/target/clonedcodechecker-1.0.0-SNAPSHOT.jar RELEASES/ccc.latest/
 mv dist/* RELEASES/ccc.latest/
